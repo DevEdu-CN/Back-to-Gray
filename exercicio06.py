4f8566c6a62b351ed1ef73f451d838108dd2cbf7
Eduardo <luiz.ed2013@gmail.com>
2022-04-03 16:46:37 -0300

    Versão Final

--- exercicio06.py
+++ exercicio06.py
@@ -1,21 +1,12 @@
 import cv2
 import numpy as np
 
-color = cv2.imread('einsteinColor.webp')
-gray = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
-grayImageBGRspace = cv2.cvtColor(gray,cv2.COLOR_GRAY2BGR)
-(thresh, blackAndWhiteImage) = cv2.threshold(grayImageBGRspace, 127, 255, cv2.THRESH_BINARY)
-horizontalAppendedIGrayImg = np.hstack((color, grayImageBGRspace, blackAndWhiteImage))
-cv2.imshow('Horizontal Appended Gray Img', horizontalAppendedIGrayImg)
+color = cv2.imread('EinsteinColor.webp')
+gray = cv2.imread('EinsteinColor.webp', 2)
 
-
-# cv2.imshow('Einstein Colorido', color)
-
-
-# horizontalAppendedIGrayImg = np.hstack((color, grayImageBGRspace))
-# cv2.imshow('Horizontal Appended Gray Img', horizontalAppendedIGrayImg)
-# cv2.imshow('Preto e Branco', blackAndWhiteImage)
-
-
+backToGray = cv2.cvtColor(gray,cv2.COLOR_GRAY2BGR)
+ret, backToBlack = cv2.threshold(backToGray, 127, 255, cv2.THRESH_BINARY)
+resp = np.concatenate((color, backToGray, backToBlack), axis = 1)
+cv2.imshow('Imagens Concatenadas', resp)
 cv2.waitKey(0)
 
